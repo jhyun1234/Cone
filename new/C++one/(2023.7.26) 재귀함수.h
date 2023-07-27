64ecaa71ffb1b5baf21cea29e1b80d919ae22a07
KIM <109838010+nohbmin@users.noreply.github.com>
2023-07-27 19:47:26 +0900

    2023.7.27.인라인 함수

---
+++ new/C++one/(2023.7.26) 재귀함수.h
@@ -0,0 +1,79 @@
+#pragma once
+#include<stdio.h>
+
+#pragma region 재귀 함수
+// 어떤 함수에서 자신을 다시 호출하여
+// 작업을 수행하는 함수이다.
+
+void Funtion()
+{
+	// 재귀 함수는 함수를 계속 호출하기 때문에 
+	// 스택 영역에 메모리가 계속 쌓이게 되므로 
+	// 스택 오버플로우가 발생한다.
+	printf("함수 호출\n");
+	Funtion();
+}
+#pragma endregion
+
+void CountDown(int count)
+{
+	// 방어 코드
+	if (count <= 0)
+	{
+		return;
+	}
+
+	printf("count의 값은 %d\n", count);
+	CountDown(count - 1);
+	printf("ㅎㅇ\n");
+}
+
+// 팩토리얼
+
+// 5!(120)  4!(24)  3!(6)
+// 3! = 3 * 2 * 1
+// 4! = 4 * 3 * 2 * 1
+// 5! = 5 * 4 * 3 * 2 * 1
+
+int Fac(int count)
+{
+	if (count <= 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return count * Fac(count - 1);
+
+	}
+
+
+}
+
+
+int main()
+{
+
+	// CountDown(5);
+
+	int data = 16;
+
+	int* ptr = &data;
+
+	// printf("%d\n", Fac(5));
+
+	// 빅 엔디안 방식
+	// 네트워크 <- 통일
+
+
+	// 리틀 엔디안 방식 
+	// 낮은 주소에 데이터의 낮은 바이트(LSB,Least,Significant Bit)
+	// 부터 저장하는 방법
+	// 비교 2byte만 하면 충분
+
+
+	// 프로그램이 정상적으로 종료되었을 때,
+	// 0 이라는 값을 반환한다.
+	return 0;
+
+}
--- new/C++one/Source.cpp
+++ new/C++one/Source.cpp
@@ -1,78 +1,69 @@
 #include<stdio.h>
 
-#pragma region 재귀 함수
-    // 어떤 함수에서 자신을 다시 호출하여
-    // 작업을 수행하는 함수이다.
+#pragma region 인라인 함수
+// 함수를 호출하는 대신 함수가 호출되는 위치마다
+// 함수의 코드를 복사하여 전달하는 방식의 함수이다.
 
-void Funtion()
-{
-	// 재귀 함수는 함수를 계속 호출하기 때문에 
-	// 스택 영역에 메모리가 계속 쌓이게 되므로 
-	// 스택 오버플로우가 발생한다.
-	printf("함수 호출\n");
-	Funtion();
-   }
+//inline void Information()
+//{
+	// int health = 100;
+	// const char* name = "Slime";
+	// printf("체력 : %d\n", health);
+	// printf("이름 : %s\n", name);
+//}
 #pragma endregion
-
-void CountDown(int count)
-{
-	// 방어 코드
-	if (count <= 0)
-	{
-		return;
-	}
-
-	printf("count의 값은 %d\n", count);
-	CountDown(count - 1);
-	printf("ㅎㅇ\n");
-}
-
-// 팩토리얼
-
-// 5!(120)  4!(24)  3!(6)
-// 3! = 3 * 2 * 1
-// 4! = 4 * 3 * 2 * 1
-// 5! = 5 * 4 * 3 * 2 * 1
-
-int Fac(int count)
-{
-	if (count <= 1)
-	{
-		return 1;
-	}
-	else
-	{
-		return count * Fac(count - 1);
-
-	}
-	
-	
-}
-
 
 int main()
 {
+#pragma region 인라인 함수
 
-	// CountDown(5);
 
-	int data = 16;
 
-	int* ptr = &data;
+	// 인라인 함수는 함수를 호출하는 과정이 없으므로 
+	// 처리 속도가 빠르지만, 인라인 함수를 많이 사용하면
+	// 험수의 코드가 복사되기 때문에 실행 파일의 크기가 커지게 된다.
+
+	// 인라인 함수의 경우 컴파일 시점에 확정되며,컴파일 시 인라인 함수로 선언하더라도
+	// 상황에 따라 일반 함수로 변환되기도 한다.
+#pragma endregion
+#pragma region 정수의 승격
+	// 일반적으로 CPU가 처리하기에 가장 적합한
+	// 크기의 정수 자료형은 int 이므로 , int형 보다
+	// 작은 자료형은 int 자료형을 변환하는 것이다.
 	
-	// printf("%d\n", Fac(5));
-
-	// 빅 엔디안 방식
-	// 네트워크 <- 통일
-
-
-	// 리틀 엔디안 방식 
-	// 낮은 주소에 데이터의 낮은 바이트(LSB,Least,Significant Bit)
-	// 부터 저장하는 방법
-	// 비교 2byte만 하면 충분
+	// char a = 10;  // 1byte
+	// short b = 20; // 2byte
 	
 
-	// 프로그램이 정상적으로 종료되었을 때,
-	// 0 이라는 값을 반환한다.
-	return 0;
+	// printf("a 변수와 b 변수를 더한 메모리 크기 : %d\n", sizeof(a +b));
+#pragma endregion
+
+#pragma region 부호없는 자료형
+
+	// char alphabet = 127; // -128 ~ 127
+	// unsigned char unAlphabet = 128; // 0 ~ 225
+	// 
+	// printf("alphabet 변수의 값 : %d\n", alphabet);
+	// printf("unalphabet 변수의 값 : %u\n", unAlphabet);
+
+	// unsigned -> "%u" 값이 다르게 나옴 
+
+	// char a = -5;
+	// unsigned char b = -5;
+	// 
+	// int data1 = a;  //4byte [1111 1111][1111 1111][1111 1111][1111 1011] <-1byte
+	// int data2 = b;  //4byte [0000 0000][0000 0000][0000 0000][1111 1011] <-1byte
+	// 
+	// printf("data1 : %d,data2의 값 : %d\n", data1, data2);
+	// printf("data1 : %u,data2의 값 : %u\n", data1, data2);
+
+#pragma endregion
+
+
+#pragma region MyRegion
+
+#pragma endregion
+
+		return 0;
 
 }
